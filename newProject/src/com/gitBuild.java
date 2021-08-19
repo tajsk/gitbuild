af270cd3a49d94d9155d4aa96ebe90b3a4671d7b
SK TAJUDDIN <SK TAJUDDIN@DESKTOP-4R9LDLO>
2021-08-19 09:29:20 +0530

    commited by sk 1

---
+++ newProject/src/com/gitBuild.java
@@ -0,0 +1,10 @@
+package com;
+
+public class gitBuild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is a jenkins build 2");
+	}
+
+}
