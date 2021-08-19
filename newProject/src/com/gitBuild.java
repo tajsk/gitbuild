59a7e72df6f667cb3ffd4c7e063e2de622b3319a
SK TAJUDDIN <SK TAJUDDIN@DESKTOP-4R9LDLO>
2021-08-19 10:24:41 +0530

    added new output

--- newProject/src/com/gitBuild.java
+++ newProject/src/com/gitBuild.java
@@ -4,7 +4,7 @@ public class gitBuild {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is a jenkins build 2");
+System.out.println("This is a jenkins build 3");
 	}
 
 }
